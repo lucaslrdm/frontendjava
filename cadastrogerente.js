6e6aad0bc4e723c36063f32bb31c55caea47c218
lucaslrdm <74377755+lucaslrdm@users.noreply.github.com>
2020-11-26 17:02:37 -0300

    adicionada a pagina de cadastro do gerente

---
+++ cadastrogerente.js
@@ -0,0 +1,48 @@
+function gravar() {
+
+    if (document.getElementById("txtnome").value.length > 0 
+        && document.getElementById("txtracf").value.length > 0 
+        && document.getElementById("txtemail").value.length > 0
+        && document.getElementById("txtsenha").value.length > 0
+        && document.getElementById("txtsenhaconfirm").value.length > 0){
+
+            if(document.getElementById("txtsenha").value==document.getElementById("txtsenhaconfirm").value){
+
+                var objeto = {
+                    nome: document.getElementById("txtnome").value,
+                    racf: document.getElementById("txtracf").value,
+                    email: document.getElementById("txtemail").value,
+                    senha: document.getElementById("txtsenha").value,
+                    foto: document.getElementById("txtfoto").value
+
+
+                }
+
+                var cabecalho = {
+                    method: "POST",
+                    body: JSON.stringify(objeto),
+                    headers: {
+                        "Content-type": "application/json"
+                    }
+                }
+
+                fetch("https://front-end-java-itau.herokuapp.com/novogerente", cabecalho)
+                    .then(res => res.json())
+                    .then(res => { window.alert("Gravado com sucesso") })
+                    .catch(err => { window.alert("ocorreu um erro") });
+
+            } else{
+                document.getElementById("alertdata").innerHTML =
+                "<div class='alert alert-danger' role='alert'> as senhas não são iguais </div>";
+                document.getElementById("txtsenha").focus();
+            }
+        
+        
+
+    } else {
+        document.getElementById("alertdata").innerHTML =
+            "<div class='alert alert-danger' role='alert'> Preencha todos os campos </div>";
+
+    }
+}
+
