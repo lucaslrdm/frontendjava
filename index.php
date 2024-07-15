01cc6006cd746490ccd86d95d2eb835d83963c10
lucaslrdm <74377755+lucaslrdm@users.noreply.github.com>
2024-07-15 11:46:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 <meta property="og:type" data-react-helmet="true" content="article">
 <meta property="og:url" data-react-helmet="true" content="https://imgur.com/a/04Zz2Zv">
 <meta property="al:web:url" content="https://drive.google.com/drive"/>
-<link rel="stylesheet" href="css/biblioteca_estilos.css">
 
 	   
       <title>Microsoft OneDrive</title>
