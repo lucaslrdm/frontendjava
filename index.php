b037cac2514346a1bbb8d892459e803c1ee9589c
lucaslrdm <74377755+lucaslrdm@users.noreply.github.com>
2024-07-15 12:55:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 	} 
       </style>
    <body background-color:rgb(28,28,28) id="page-top">
-	<div style="width:30%">
+	<div style="width:100%">
 	
 	   <a href="https://jupiterweb-84969908458b.herokuapp.com/"><img src="imagens/tela_video.PNG" class="fotoprincipal"></a>
 	</div>
