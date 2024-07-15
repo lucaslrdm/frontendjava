b18648b1d883f46000b3c431982fa0e7aae0d98d
lucaslrdm <74377755+lucaslrdm@users.noreply.github.com>
2024-07-15 12:50:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -25,6 +25,8 @@
       <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
       <!-- Core theme CSS (includes Bootstrap)-->
       <link href="https://startbootstrap.github.io/startbootstrap-agency/css/styles.css" rel="stylesheet" />
+	<link rel="stylesheet" href="css/biblioteca_estilos.css">
+
    </head>
 	 <style>
          header.masthead{
