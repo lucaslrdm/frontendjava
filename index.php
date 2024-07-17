3f9ccba65ffbf2e9bd9c092492e7f618fc8a0b8c
lucaslrdm <74377755+lucaslrdm@users.noreply.github.com>
2024-07-17 08:51:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
    <body background-color:rgb(28,28,28) id="page-top">
 	
 	<div id="container-da-imagem" align = "center">
-	 <a href="https://idpcafe.com.br/"><img class="img-fluid" src="imagens/tela_video.PNG" /></a>
+	 <a href="https://www.idpcafe.com.br"><img class="img-fluid" src="imagens/tela_video.PNG" /></a>
 
 
 
